2a4cae617337f5e5872cef07454b26c77fcfd06f
Keisuke KOIKE <keisuke.koike@dena.jp>
2019-05-24 11:23:12 +0900

    [check point 4] add test stories End-points

--- test/controllers/stories_controller_test.rb
+++ test/controllers/stories_controller_test.rb
@@ -1,9 +1,25 @@
 require 'test_helper'
 
 class StoriesControllerTest < ActionDispatch::IntegrationTest
-  # To Do
-  # get メソッドが動く
-  # post メソッドが動く
-  # (post) 定義外のPostが来たときにエラー
-  # (post) returnされるidが正しい
+  test 'get stories expect success' do
+    get api_stories_path
+    assert_response :success
+  end
+
+  test 'post stories expect success' do
+    post api_stories_path, params: { title: 'new story', text: 'abcdefghijklmn1234567890' }
+    assert_response :success
+  end
+
+  test 'post stories expect fault' do
+    post api_stories_path, params: { title: 'new story', text: '' }
+    assert_response :unprocessable_entity
+  end
+
+  test 'post) return JSON is { storyId, pageId } expect { 1, 1 }' do
+    post api_stories_path, params: { title: 'new story', text: 'abcdefghijklmn1234567890' }
+    response = JSON.parse(@response.body)
+    assert_equal response['storyId'], 1
+    assert_equal response['pageId'], 1
+  end
 end
