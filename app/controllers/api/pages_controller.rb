e6e683af5c4375d2c89879920d3906569da57dc6
Keisuke KOIKE <keisuke.koike@dena.jp>
2019-05-24 16:48:33 +0900

    [check point 6] add getPages end-point

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -9,10 +9,9 @@ class Api::PagesController < ApplicationController
         parent_id: page_params[:page_id]
       )
       if page.save
-        render json: { pageId: page.id }, status: 201
-      else
-        render json: { error: 'Validation error' }, status: 422
+        return render json: { id: page.id }, status: 201
       end
+      render json: { error: 'Validation error' }, status: 422
     else
       render json: { error: 'Given story_id or page_id is not found '}, status: 404
     end
@@ -20,16 +19,12 @@ class Api::PagesController < ApplicationController
 
   # getPage end-point
   def show
-    results = Page.select('id, name, text').where(id: page_params[:page_id], story_id: page_params[:story_id])
-    if results.exists?
-      render json: { error: 'Given story_id or page_id is not found '}, status: 404
-    end
-    children = Page.select('id, name').where(parent_id: page_params[:page_id])
-    render json: results.to_json(children: children), status: 200
+    page = Page.find(params[:id]).as_json(include: [:children])
+    render json: page, status: 200
   end
 
   private
   def page_params
-    params.permit(:story_id, :page_id, :name, :text)
+    params.permit(:id, :page_id, :story_id, :name, :text)
   end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,5 @@
 class Page < ApplicationRecord
+  has_many :children, class_name: 'Page', foreign_key: 'parent_id'
   belongs_to :story
 
   validates :name, presence: true
